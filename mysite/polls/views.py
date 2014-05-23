cf0062f613512d892621bd016b1a4b838c10934d
System Administrator <root@Jimmy-XP-PCs-Mac-mini.local>
2014-05-23 11:44:18 +0800

    django demo

---
+++ mysite/polls/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.views import generic
+from polls.models import Choice, Poll
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+# Create your views here.
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+#    context = RequestContext(request, {'latest_poll_list': latest_poll_list,})
+#    output = ', '.join([p.question for p in lastest_poll_list])
+    context = {"latest_poll_list": latest_poll_list}
+    return render(request, 'polls/index.html', context)
+#    return HttpResponse(template.render(context))
+
+def detail(request, poll_id):
+    """
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
+    """
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
